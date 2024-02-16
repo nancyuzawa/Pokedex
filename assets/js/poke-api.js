2f4e761c217f915d8224ecaa31cee13f28751d43
Nah~ <110066948+nancyuzawa@users.noreply.github.com>
2024-02-16 18:56:29 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -61,11 +61,11 @@ pokeApi.getPokemons = (offset = 0, limit = 151) => {
     .then((pokemonsDetails) => pokemonsDetails)
 }
 
-// Promise.all([
-//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
-//     fetch('https://pokeapi.co/api/v2/pokemon/4')
-// ]).then(results=> {
-//   // console.log(results)
-// })
+ Promise.all([
+     fetch('https://pokeapi.co/api/v2/pokemon/1'),
+     fetch('https://pokeapi.co/api/v2/pokemon/2'),
+     fetch('https://pokeapi.co/api/v2/pokemon/3'),
+     fetch('https://pokeapi.co/api/v2/pokemon/4')
+ ]).then(results=> {
+    console.log(results)
+ })
